f56c1437a1e026108dbfe7af91f0326ade339c49
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2024-04-02 19:47:28 -0300

    Avance seccion 18 - 252

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.requiredTrue ]],
+    password: ['', [ Validators.requiredTrue, Validators.minLength(6) ]],
+    password2: ['', [ Validators.requiredTrue ]],
+  });
+
+  constructor( private fb: FormBuilder ){}
+
+  isValidField( field: string ){
+    // TODO: obtener validacion desde un servicio
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 
 }
